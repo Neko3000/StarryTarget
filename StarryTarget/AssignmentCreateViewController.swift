4035c03912742ed036b42221315300ac730f2698
Neko3000 <Neko3000@live.com>
2018-01-21 23:04:55 +0900

    feat(AssignmentCreateViewController): complete constraints of the AssignmentCreateController's view in the stroyboard

---
+++ StarryTarget/AssignmentCreateViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AssignmentCreateViewController.swift
+//  StarryTarget
+//
+//  Created by Xueliang Chen on 1/21/18.
+//  Copyright © 2018 Conceptual. All rights reserved.
+//
+
+import UIKit
+
+class AssignmentCreateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
