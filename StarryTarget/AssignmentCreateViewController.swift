edffc344df6c8d10aaea5847daa729a1cfd6eeb6
Neko3000 <Neko3000@live.com>
2018-02-01 10:01:04 +0900

    feat(AssignmentCreateViewController): add Timer to AssignmentCreateViewController, add obserable variables StartTime and EndTime to responsible for updating UILabel

--- StarryTarget/AssignmentCreateViewController.swift
+++ StarryTarget/AssignmentCreateViewController.swift
@@ -14,8 +14,17 @@ class AssignmentCreateViewController: UIViewController {
     
     @IBOutlet weak var MainTimePickerView: TimePickerView!
     
+    
+    @IBOutlet weak var StartPeriodLabel: UILabel!
+    @IBOutlet weak var StartTimeLabel: UILabel!
+    
     @IBOutlet weak var EndPeriodLabel: UILabel!
     @IBOutlet weak var EndTimeLabel: UILabel!
+    
+    private var timer:Timer?
+    
+    private var StartTime:Variable<Date> = Variable<Date>(Date())
+    private var EndTime:Variable<Date> = Variable<Date>(Date())
     
     var disposeBag = DisposeBag()
     
@@ -24,35 +33,58 @@ class AssignmentCreateViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         
+        //gesture - tap EndTimeLabel to show TimePickerView
         let tapBehavior = UITapGestureRecognizer(target: self, action:#selector(self.showDatePicker))
         EndTimeLabel.isUserInteractionEnabled = true
         EndTimeLabel.addGestureRecognizer(tapBehavior)
         
         //observer UILabs
-        MainTimePickerView.time.asObservable().map({t -> String? in
-            let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "HH:mm"
+        StartTime.asObservable().map({t -> String in
+            let dateFormatterForPeriod = DateFormatter()
+            dateFormatterForPeriod.dateFormat = "a"
             
-            let targetTime = Date().addingTimeInterval(TimeInterval(t.hour!*60*60 + t.minute!*60 + t.second!))
-            let targetTimeString = dateFormatter.string(from: targetTime)
-            
-            return targetTimeString
-        }).bind(to: EndTimeLabel.rx.text).disposed(by: self.disposeBag)
+            return dateFormatterForPeriod.string(from: t)
+        }).bind(to:StartPeriodLabel.rx.text).disposed(by: self.disposeBag)
         
-        MainTimePickerView.time.asObservable().map({t -> String? in
+        StartTime.asObservable().map({t -> String in
+            let dateFormatterForTime = DateFormatter()
+            dateFormatterForTime.dateFormat = "HH:mm"
+            
+            return dateFormatterForTime.string(from: t)
+        }).bind(to:StartTimeLabel.rx.text).disposed(by: self.disposeBag)
+        
+        MainTimePickerView.time.asObservable().map({t -> Date in
+            let targetTime = Date().addingTimeInterval(TimeInterval(t.hour!*60*60 + t.minute!*60 + t.second!))
+            
+            return targetTime
+        }).bind(to: EndTime).disposed(by: self.disposeBag)
+        
+        EndTime.asObservable().map({t -> String in
             let dateFormatter = DateFormatter()
             dateFormatter.dateFormat = "a"
             
-            let targetTime = Date().addingTimeInterval(TimeInterval(t.hour!*60*60 + t.minute!*60 + t.second!))
-            
-            return dateFormatter.string(from: targetTime)
+            return dateFormatter.string(from: self.EndTime.value)
         }).bind(to: EndPeriodLabel.rx.text).disposed(by: self.disposeBag)
         
+        EndTime.asObservable().map({t -> String in
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "HH:mm"
+            
+            return dateFormatter.string(from: self.EndTime.value)
+        }).bind(to: EndTimeLabel.rx.text).disposed(by: self.disposeBag)
+        
+        //set timer
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateCurrentTime), userInfo: nil, repeats: true)
     }
     
     @objc func showDatePicker(sender:UITapGestureRecognizer)
     {
         MainTimePickerView.isHidden = false
+    }
+    
+    @objc func updateCurrentTime()
+    {
+        StartTime.value = Date()
     }
 
     override func didReceiveMemoryWarning() {
